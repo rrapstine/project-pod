10b622a0df40f387285d17d38234e8916fd2b5c7
Richard F Rapstine III <richard.rapstine@gmail.com>
2025-10-01 22:02:39 -0500

    refactor(api): Update API routing to use v1 prefix

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
             Route::middleware('api')
                 ->prefix('v1')
                 ->group(base_path('routes/api/v1.php'));
